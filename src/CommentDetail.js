56c8628501404bbe3070d23dcbc5f962668ac1fb
Unknown <xin_wang15@hotmail.com>
2018-12-08 21:20:07 -0500

    Dec 8

---
+++ src/CommentDetail.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+
+const CommentDetail =props =>{
+	return(
+		<div className="comment">
+  	      <a href="/" className="avatar">
+  	      	<img alt="avatar" src={props.src}/>
+		  </a>
+		  <div className="content">
+		  	<a href="/" className="author">
+		  	{props.author}
+		  	</a>
+		  	<div className="metadata">
+		  	  <span className="data">{props.timeAgo}</span>
+		  	</div>
+		  	<div className="text">{props.body}</div>
+		  </div>
+  	  </div>
+
+
+
+	);
+};
+
+export default CommentDetail;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import faker from 'faker';
+import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
+const App = () => {
+  return (
+  	<div className="ui container comments">
+  	   <ApprovalCard>
+  	   <CommentDetail author="Sam"timeAgo="Today at 2:00 PM" body="Have a nice holiday"src={faker.image.avatar()}/>
+  	   </ApprovalCard>
+  	   <ApprovalCard>
+  	   <CommentDetail author="Alex" timeAgo="Today at 5:00 PM" body="I hate holiday"src={faker.image.avatar()}/>
+  	   </ApprovalCard>
+  	   <ApprovalCard>
+  	   <CommentDetail author="Jane"timeAgo="Today at 10:45 AM" body="I wish I have more holiday"src={faker.image.avatar()}/>
+  	   </ApprovalCard>
+  	</div>
 
-ReactDOM.render(<App />, document.getElementById('root'));
+   );
+}; 
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(<App />, document.querySelector('#root'));
